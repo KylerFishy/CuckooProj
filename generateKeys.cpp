a75725a4005a02248658f529d8ac6013551a7491
KylerFishy <45405216+KylerFishy@users.noreply.github.com>
2018-12-03 21:10:11 -0800

    Add files via upload

---
+++ generateKeys.cpp
@@ -0,0 +1,15 @@
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  ofstream myfile;
+  srand(time(NULL));
+  myfile.open ("keys2.txt");
+  for(int i=0; i<1000;i++) myfile <<rand() <<endl;
+  myfile.close();
+
+   return 0;
+}
